dc04374e894028bd8f53afeafc1a3b87e6337fee
haroldrubio <harrywill.rubio@gmail.com>
2021-07-12 09:06:14 -0400

    Update run_qa.py

--- scripts/run_qa.py
+++ scripts/run_qa.py
@@ -2,7 +2,7 @@ __requires__ = "autoregkd"
 import re
 import sys
 
-from autoregkd.training.__main__ import run_qa
+from src.autoregkd.training.run_qa import main as run_qa
 
 if __name__ == "__main__":
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
